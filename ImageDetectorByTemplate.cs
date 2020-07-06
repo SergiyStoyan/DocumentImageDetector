91fd72af59ed34d445353bc5dfd5f30ea1e17ec6
Sergey Stoyan <sergey.stoyan@gmail.com>
2020-07-06 19:02:13 +0300

    .

--- ImageDetectorByTemplate.cs
+++ ImageDetectorByTemplate.cs
@@ -40,6 +40,44 @@ namespace testImageDetection
             return result;
         }
 
+        static public void FindMatchs(string pageFile, string templateFile, Size padding)
+        {
+            Image<Rgb, byte> pageRgbImage = new Image<Rgb, byte>(pageFile);
+            Image<Rgb, byte> templateRgbImage = new Image<Rgb, byte>(templateFile);
+            using (Image<Gray, byte> pageImage = getPreprocessedImage(pageFile))
+            {
+                using (Image<Gray, byte> templateImage = getPreprocessedImage(templateFile))
+                {
+                    using (Image<Gray, float> match = pageImage.MatchTemplate(templateImage, TemplateMatchingType.CcoeffNormed))
+                    {
+                        float[,,] matches = match.Data;
+                        List<Rectangle> matchRs = new List<Rectangle>();
+                        for (int x = matches.GetLength(1) - 1; x >= 0; x--)
+                        {
+                            for (int y = matches.GetLength(0) - 1; y >= 0; y--)
+                            {
+                                double matchScore = matches[y, x, 0];
+                                if (matchScore > 0.91)
+                                {
+                                    Rectangle r = new Rectangle(new Point(x, y), templateImage.Size);
+                                    var d = matchRs.FirstOrDefault(a => a.Contains(r));
+                                    if (matchRs.FirstOrDefault(a => a.Contains(r)) == Rectangle.Empty)
+                                    {
+                                        pageRgbImage.Draw(r, new Rgb(255, 0, 0), 1);
+                                        r.Inflate(padding);
+                                        matchRs.Add(r);
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+            MainForm.This.PageBox.Image = pageRgbImage.ToBitmap();
+            MainForm.This.TemplateBox.Image = templateRgbImage.ToBitmap();
+        }
+
         static private Image<Gray, byte> getPreprocessedImage(string imageFile)
         {
             Image<Gray, byte> image = new Image<Gray, byte>(imageFile);
--- MainForm.cs
+++ MainForm.cs
@@ -22,10 +22,11 @@ namespace testImageDetection
             InitializeComponent();
 
             Load += delegate {
-                ImageDetectorByContour idbc = new ImageDetectorByContour(@"c:\\temp\bt.png");
-                idbc.FindOnPage(@"c:\\temp\b2.png");
+                //ImageDetectorByContour idbc = new ImageDetectorByContour(@"c:\\temp\bt.png");
+                //idbc.FindOnPage(@"c:\\temp\b2.png");
                 //ImageDetectorByKeyPoints.FindMatch(@"c:\\temp\pp3.png", @"c:\\temp\ppt.png");
-                //ImageDetectorByTemplate.FindMatch(@"c:\\temp\b.png", @"c:\\temp\bt.png");
+                //ImageDetectorByTemplate.FindMatch(@"c:\\temp\test1.png", @"c:\\temp\t1.png");
+                ImageDetectorByTemplate.FindMatchs(@"c:\\temp\test1.png", @"c:\\temp\t1.png", new Size(10,10));
             };
         }
 
